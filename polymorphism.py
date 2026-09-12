4b467b614665bb3342ff7a2ece184a34d08d1d06
ahmedsyedhaashim-web <ahmedsyedhaashim@gmail.com>
2026-09-12 19:25:01 +0800

    file handling

--- polymorphism.py
+++ polymorphism.py
@@ -1,11 +1,12 @@
 class cricket:
-    def__init_(self, player, score):
+     def__init_(self, player, score):
         self.player = player
         self.score = score
     def info(self):
         print(f'cricket - player: {self.__player}, Score: {self.__score}"end)
     def play(self):
         print(f"{self.__player} hits a six!")
+        
     def get_score(self):
         return self.__score
     def set_score(self, new_score):
@@ -13,7 +14,4 @@ class cricket:
             self.__score = new_score
             print(f"score updated to {self.__score}")
             else:
-            print("score cannot be negative")
-
-class football:
-def __init__(self, player, score):
+            print("score cannot be negat
---
+++ test.py
@@ -0,0 +1,18 @@
+class Test: 
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        return f"Hello, {self.name}!"\
+    
+
+
+
+
+
+
+
+
+
+
+
